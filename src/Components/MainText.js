90c8352dd3b0c319ee124e52e7139bfec8f26b49
Ginura Premawardana <ginujj9@gmail.com>
2024-03-29 12:42:18 +0530

    Main page content adding part 1

---
+++ src/Components/MainText.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './MainText.css'
+
+export default function MainText() {
+  return (
+    <div className='MainText1'>
+      <h1>Your Imagination Is</h1>
+      <h1>Your Only Limit</h1>
+
+      <p>We always try to make our customer Happy. We provide all kind of facilities. Your Satisfaction is our main priority</p>
+
+      <a href='#'>Discover More</a>
+
+
+    
+    </div>
+  )
+}
---
+++ src/Components/Nav.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import './Nav.css'
+
+export default function Nav() {
+  return (
+    <div className='nav'>
+      <div className="NavItems">
+        <div className="navItemsL">
+            <p>Home</p>
+            <p>About Us</p>
+        </div>
+        <div className="navItemsR">
+            <p>Icon1</p>
+            <p>Icon2 Us</p>
+        </div>
+      
+      </div>
+    </div>
+  )
+}
